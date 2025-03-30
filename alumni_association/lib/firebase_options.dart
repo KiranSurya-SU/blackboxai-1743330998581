900bc2a2cb5b667b8838356dbdf1b2d125643d69
Kiran Surya <146125805+KiranSurya-SU@users.noreply.github.com>
2025-03-30 16:06:38 +0530

    Add alumni_association/lib/firebase_options.dart 1743331002522

---
+++ alumni_association/lib/firebase_options.dart
@@ -0,0 +1,13 @@
+// This file will be auto-generated when you run the FlutterFire CLI.
+// For now, it serves as a placeholder.
+
+class DefaultFirebaseOptions {
+  static const FirebaseOptions currentPlatform = FirebaseOptions(
+    apiKey: 'YOUR_API_KEY',
+    appId: 'YOUR_APP_ID',
+    messagingSenderId: 'YOUR_SENDER_ID',
+    projectId: 'YOUR_PROJECT_ID',
+    authDomain: 'YOUR_AUTH_DOMAIN',
+    storageBucket: 'YOUR_STORAGE_BUCKET',
+  );
+}
